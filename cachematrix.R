a4404cb6aa44eb415a446c85cea3b995e6c21561
Ray Lugo, Jr <rlugojr@gmail.com>
2016-10-29 15:27:13 -0400

    cacheSolve and descriptions
    Finished first version of cacheSolved and updated descriptions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,8 @@
-## These functions are used to optimize the
-## computation of the inverse of a matrix by
-## accepting a matrix as a parameter, calculating
-## its inverse, saving the inverse in a closure
+## These functions are used to optimize the computation of the inverse of a matrix by
+## accepting a matrix as a parameter, calculating its inverse, saving the inverse in a closure
 ## and returning the cached value if passed the same matrix.
-## A new matrix would, require calculation and would also
-## replace the cached value.
+## A new matrix would, require calculation and would also set the cached value of the matrix.
+
 
 ## makeCacheMatrix() is a matrix object with an inner closure that acts as simple cache
 ## for the matrix and its inverse.
@@ -28,11 +26,13 @@ makeCacheMatrix <- function(checkMatrix = matrix()) {
 ## the cache.
 
 cacheSolve <- function(m, ...) {
-  cachedVal <- m$getInverse
-  if (!is.null(cachedVal)){
+  cachedVal <- m$getInverse()
+  if (!is.null(cachedVal)) {
       message("using cached data")
       return(cachedVal)
   }
-
-
+  matrix_data <- m$getMatrix()
+  matrix_inverse <- solve(matrix_data, ...)
+  m$setInverse(matrix_inverse)
+  return(matrix_inverse)
 }
