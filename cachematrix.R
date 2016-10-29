22f11c07fdbf903c615a2781b7b18d953ebda83b
Ray Lugo, Jr <rlugojr@gmail.com>
2016-10-29 15:44:25 -0400

    Final version
    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -7,14 +7,22 @@
 ## makeCacheMatrix() is a matrix object with an inner closure that acts as simple cache
 ## for the matrix and its inverse.
 makeCacheMatrix <- function(checkMatrix = matrix()) {
+    #instantiate object with checkMatrix parameter.
     invMatrix <- NULL
     setMatrix <- function(newMatrix) {
         checkMatrix <<- newMatrix
         invMatrix <<- NULL
     }
+    #function to check if matrix has been solved and result cached.
     getMatrix <- function() checkMatrix
+
+    #function to store the inverse of the matrix.
     setInverse <- function(InverseMatrix) invMatrix <<- InverseMatrix
+
+    #function to retrieve the cached inverse of the matrix.
     getInverse <- function() invMatrix
+
+    #expose the getters and setters
     list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
@@ -24,11 +32,15 @@ makeCacheMatrix <- function(checkMatrix = matrix()) {
 ## the cache.
 
 cacheSolve <- function(m, ...) {
+    #check for a cached result for this matrix
     cachedVal <- m$getInverse()
+
+    #if a non-nul value is returned from the cache then use it.
     if (!is.null(cachedVal)) {
         message("using cached data")
         return(cachedVal)
     }
+    #if NULL is returned then calculate and store the inverse of the matrix.
     matrix_data <- m$getMatrix()
     matrix_inverse <- solve(matrix_data, ...)
     m$setInverse(matrix_inverse)
