04893e599b4ef226539d702548a722b3cf84dc63
Ray Lugo, Jr <rlugojr@gmail.com>
2016-10-29 15:34:17 -0400

    StyleGuide and Description change
    Formatted code to use 4 spaced indents, instead of 2 spaces, for
    readability.  Updated descriptions.

--- cachematrix.R
+++ cachematrix.R
@@ -7,32 +7,30 @@
 ## makeCacheMatrix() is a matrix object with an inner closure that acts as simple cache
 ## for the matrix and its inverse.
 makeCacheMatrix <- function(checkMatrix = matrix()) {
-  invMatrix <- NULL
-  setMatrix <- function(newMatrix) {
-    checkMatrix <<- newMatrix
-    invMatrix <<- NULL
-  }
-  getMatrix <- function() checkMatrix
-  setInverse <- function(InverseMatrix) invMatrix <<- InverseMatrix
-  getInverse <- function() invMatrix
-  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
+    invMatrix <- NULL
+    setMatrix <- function(newMatrix) {
+        checkMatrix <<- newMatrix
+        invMatrix <<- NULL
+    }
+    getMatrix <- function() checkMatrix
+    setInverse <- function(InverseMatrix) invMatrix <<- InverseMatrix
+    getInverse <- function() invMatrix
+    list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## cacheSolve() takes the matrix object makeCacheMatrix()
-## and checks for the cached inverse of the matrix.
-## Then either the cached version is returned or it
-## calulates the inverse matrix and stores the result in
+## cacheSolve() takes the matrix object makeCacheMatrix() and checks for the cached inverse of the matrix.
+## Then either the cached version is returned or it calulates the inverse matrix and stores the result in
 ## the cache.
 
 cacheSolve <- function(m, ...) {
-  cachedVal <- m$getInverse()
-  if (!is.null(cachedVal)) {
-      message("using cached data")
-      return(cachedVal)
-  }
-  matrix_data <- m$getMatrix()
-  matrix_inverse <- solve(matrix_data, ...)
-  m$setInverse(matrix_inverse)
-  return(matrix_inverse)
+    cachedVal <- m$getInverse()
+    if (!is.null(cachedVal)) {
+        message("using cached data")
+        return(cachedVal)
+    }
+    matrix_data <- m$getMatrix()
+    matrix_inverse <- solve(matrix_data, ...)
+    m$setInverse(matrix_inverse)
+    return(matrix_inverse)
 }
